f47178f905c272e1c0ba724d3ee3e04fed33060f
chlorocodes <chlorocodestv@gmail.com>
2023-07-24 12:55:12 -0400

    translation updates

--- server/src/services/bot/commands/translate.ts
+++ server/src/services/bot/commands/translate.ts
@@ -1,4 +1,9 @@
-import { Message, APIEmbed } from 'discord.js'
+import {
+  Message,
+  APIEmbed,
+  GuildTextBasedChannel,
+  TextBasedChannel
+} from 'discord.js'
 import { googleService } from '../../google'
 
 type TranslatedMessage = Pick<Message, 'author' | 'content'>
@@ -9,42 +14,60 @@ export async function translate(message: Message, args: string[] = []) {
   }
   let untranslatedText = args.join(' ').trim()
   if (!untranslatedText) {
-    const reference = await message.fetchReference()
-    untranslatedText = reference.content
+    if (message.reference) {
+      const reference = await message.fetchReference()
+      untranslatedText = reference.content
+    } else {
+      const [previousMessage] = await getMessages(
+        message.channel,
+        1,
+        message.id
+      )
+      untranslatedText = previousMessage.content
+    }
   }
   const translation = await googleService.translate(untranslatedText)
   message.reply(`Translation: ${translation}`)
 }
 
 async function multiTranslate(message: Message) {
-  const limit = Number(message.content.split('~')[1])
-
-  if (!limit || Number.isNaN(limit)) {
+  const count = Number(message.content.split('~')[1])
+  if (!count || Number.isNaN(count)) {
     return message.reply('Invalid usage')
   }
-
-  if (limit > 10) {
+  if (count > 10) {
     return message.reply(
       'You can only translate a maximum of 10 messages at a time'
     )
   }
-
-  const collection = await message.channel.messages.fetch({
-    limit,
-    before: message.id
-  })
-  const messages = [...collection.values()].reverse()
-  const messagesToTranslate = messages.filter(
-    (message) => message.content.trim() !== ''
-  )
+  const channel = message.channel
+  const untranslatedMessages = await getMessages(channel, count, message.id)
   const translatedMessages: TranslatedMessage[] = await Promise.all(
-    messagesToTranslate.map(async (message) => ({
+    untranslatedMessages.map(async (message) => ({
       author: message.author,
       content: await googleService.translate(message.content)
     }))
   )
   const messageEmbed = createEmbed(translatedMessages)
   message.channel.send({ embeds: [messageEmbed] })
+}
+
+async function getMessages(
+  channel: GuildTextBasedChannel | TextBasedChannel,
+  count: number,
+  originalMessageId: string
+) {
+  const collection = await channel.messages.fetch({
+    limit: count,
+    ...(originalMessageId && {
+      before: originalMessageId
+    })
+  })
+  const messages = [...collection.values()].reverse()
+  const untranslatedMessages = messages.filter(
+    (message) => message.content.trim() !== ''
+  )
+  return untranslatedMessages
 }
 
 function createEmbed(translatedMessages: TranslatedMessage[]) {

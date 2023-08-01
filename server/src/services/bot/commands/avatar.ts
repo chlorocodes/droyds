825e11995793c2a132df99ff256e1f50c9c08598
chlorocodes <chlorocodestv@gmail.com>
2023-08-01 16:47:56 -0700

    Reply to user when avatar is requested

--- server/src/services/bot/commands/avatar.ts
+++ server/src/services/bot/commands/avatar.ts
@@ -3,7 +3,7 @@ import { Message } from 'discord.js'
 export async function avatar(message: Message) {
   const user = message.mentions?.members?.at(0) ?? message.author
   const avatarUrl = user.displayAvatarURL() + '?size=512'
-  await message.channel.send({
+  await message.reply({
     files: [{ attachment: avatarUrl }]
   })
 }

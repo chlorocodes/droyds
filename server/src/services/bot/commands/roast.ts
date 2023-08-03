98f77f1d609901dfa360daa97c5aa3247dc0a985
chlorocodes <chlorocodestv@gmail.com>
2023-08-03 07:49:11 -0700

    Allow chloro to be roasted

--- server/src/services/bot/commands/roast.ts
+++ server/src/services/bot/commands/roast.ts
@@ -1,28 +1,19 @@
 import { Message } from 'discord.js'
 import { roaster } from '../../roaster'
-import { botInfo } from '../config/bot-info'
-import { complimenter } from '../../complimenter'
 
 const greetings = ['Yo', 'Hey', 'Sup']
 
 export async function roast(message: Message) {
   const victim = message.mentions?.members?.at(0) ?? message.author
   const greeting = greetings[Math.floor(Math.random() * greetings.length)]
-
-  let response: string
-
-  if (victim.id === botInfo.adminId) {
-    response = await complimenter.compliment()
-  } else {
-    response = await roaster.roast()
-  }
+  const insult = await roaster.roast()
 
   if (victim.id === message.author.id) {
-    return message.channel.send(response)
+    return message.channel.send(insult)
   }
 
   const formattedInsult = `${greeting} <@${victim.id}> – ${
-    response[0].toLowerCase() + response.slice(1)
+    insult[0].toLowerCase() + insult.slice(1)
   }`
 
   message.channel.send(formattedInsult)

4f4cb967b4064877eedc5a93bc5013b65f0f672d
chlorocodes <chlorocodestv@gmail.com>
2023-08-04 15:55:14 -0700

    no lyme roasting

--- server/src/services/bot/commands/roast.ts
+++ server/src/services/bot/commands/roast.ts
@@ -1,10 +1,16 @@
 import { Message } from 'discord.js'
 import { roaster } from '../../roaster'
+import { botInfo } from '../config/bot-info'
 
 const greetings = ['Yo', 'Hey', 'Sup']
 
 export async function roast(message: Message) {
   const victim = message.mentions?.members?.at(0) ?? message.author
+
+  if (victim.id === botInfo.id) {
+    return message.reply('no')
+  }
+
   const greeting = greetings[Math.floor(Math.random() * greetings.length)]
   const insult = await roaster.roast()
 
